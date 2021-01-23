4f0272955ba2c52ae950696fe870eb552e566b06
mkovachev <mkovachev6@gmail.com>
2021-01-23 19:20:36 +0200

    added properties

--- src/app/animal/animal.model.ts
+++ src/app/animal/animal.model.ts
@@ -1,11 +1,8 @@
 import { Type } from '@angular/core';
-import { Optional } from '@angular/core';
 
 
 export class Animal {
     constructor(
-        public data: any,
-    ) { }
-
-    type!: Type<any>
+        public type: Type<any>,
+        public data: any) { }
 }
