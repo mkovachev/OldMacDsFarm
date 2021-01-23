1333b2ccfc973f98181a8773f27ef196a8a0d874
mkovachev <mkovachev6@gmail.com>
2021-01-23 19:21:02 +0200

    added some animals

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AnimalComponent } from '../animal/animal.component';
 import { Animal } from '../animal/animal.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AnimalService {
   getAnimals() {
     return [
-      new Animal({ name: 'Cow', sound: 'moo' }),
-      new Animal({ name: 'Pig', sound: 'oink' }),
-      new Animal({ name: 'Rooster', sound: 'crows' }),
-      new Animal({ name: 'Donkey', sound: 'hee-haw' }),
-      new Animal({ name: 'Chicken', sound: 'cluck' })
+      new Animal(AnimalComponent, { name: 'Cow', sound: 'moo', image: "../../assets/images/cow.png" }),
+      new Animal(AnimalComponent, { name: 'Pig', sound: 'oink', image: "../../assets/images/pig.png" }),
+      new Animal(AnimalComponent, { name: 'Rooster', sound: 'crows', image: "../../assets/images/rooster.gif" }),
+      new Animal(AnimalComponent, { name: 'Donkey', sound: 'hee-haw', image: "../../assets/images/donkey.png" }),
+      new Animal(AnimalComponent, { name: 'Chicken', sound: 'cluck', image: "../../assets/images/chicken.png" })
     ];
   }
 }
