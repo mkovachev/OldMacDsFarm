d5d24a074489428ee423fce98efce99e080cb378
mkovachev <mkovachev6@gmail.com>
2021-01-23 16:00:46 +0200

    implemets animal

--- src/app/animal/animal.component.ts
+++ src/app/animal/animal.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
   styleUrls: ['./animal.component.css']
 })
-export class AnimalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AnimalComponent implements Animal {
+  data: any
 }
