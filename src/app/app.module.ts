786dbe331457da56460a4fa498ccfbc6f4dad620
mkovachev <mkovachev6@gmail.com>
2021-01-23 15:37:01 +0200

    farm will handle main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FarmComponent } from './farm/farm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FarmComponent
   ],
   imports: [
     BrowserModule,
