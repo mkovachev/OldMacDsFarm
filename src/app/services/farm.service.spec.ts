0486c5fb73309897ed3f37a44a40aaec5d8ff8bf
mkovachev <mkovachev6@gmail.com>
2021-01-26 20:51:06 +0200

    created

--- src/app/services/farm.service.spec.ts
+++ src/app/services/farm.service.spec.ts
@@ -1,18 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Animal } from '../models/animal.model';
 import { FarmService } from './farm.service';
 
 describe('FarmService', () => {
-  let service: FarmService;
-
+  let service: FarmService
+  const custom = []
+  const animal = new Animal('breed', 'sound', '')
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(FarmService);
-  });
-  
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(FarmService)
+  })
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-  
-  
-});
+    expect(service).toBeTruthy()
+  })
+
+  // describe("create function should add new animal into custom array", () => {
+  //   return service.create(animal), result! => {
+  //     expect(result.length).toBeGreaterThan(0);
+  //   })
+  // })
+
+})
