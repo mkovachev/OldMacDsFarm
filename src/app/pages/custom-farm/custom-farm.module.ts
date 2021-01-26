6c411bb2f4cb5c9dd96b5727dd4795b00569321b
mkovachev <mkovachev6@gmail.com>
2021-01-26 11:19:12 +0200

    re-added routermodule

--- src/app/pages/custom-farm/custom-farm.module.ts
+++ src/app/pages/custom-farm/custom-farm.module.ts
@@ -4,13 +4,15 @@ import { CreateComponent } from './create/create.component';
 import { NgStackFormsModule } from '@ng-stack/forms';
 import { CustomRenderComponent } from './render/custom-render.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CreateComponent, CustomRenderComponent],
   imports: [
     CommonModule,
     NgStackFormsModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class CustomFarmModule { }
--- src/app/pages/my-farm/my-farm.module.ts
+++ src/app/pages/my-farm/my-farm.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyFarmRenderComponent } from './render/myfarm-render.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MyFarmRenderComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class MyFarmModule { }
